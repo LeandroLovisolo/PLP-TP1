4f95739c62e215fe2b87b9db66aa9cf490fbe454
Leandro Lovisolo <leandro@leandro.me>
2014-09-07 02:30:32 -0300

    Comentarios ejercicio 7

--- src/mapReduce.hs
+++ src/mapReduce.hs
@@ -127,6 +127,14 @@ distributionProcess n = reverse . foldr addToFirstAndRotate (replicate n [])
         rotate (x:xs)                = xs ++ [x]
 
 -- Ejercicio 7
+-- Aplica una función mapper 'f' a los elementos de una lista y agrupa los resultados
+-- por clave. Para lograr esto primero aplica 'f' a todos los elementos de la lista
+-- original con la función 'map', luego aplana la lista de listas de tuplas resultante
+-- con 'concat' y finalmente agrupa por clave aplicando la función 'groupByKey'.
+--
+-- Ejemplo:
+-- *MapReduce> mapperProcess (\x -> [(if x `mod` 2 == 0 then "par" else "impar", x)]) [1..5]
+-- [("impar",[1,3,5]),("par",[2,4])]
 mapperProcess :: Eq k => Mapper a k v -> [a] -> [(k,[v])]
 mapperProcess f = groupByKey . concat . map f
 
